4f15edc0b31d18b1891398925a26d772d3cb065d
bodhi.root <bodhi.root@gmail.com>
2026-02-27 17:44:55 -0500

    Updated links to use base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bodhi-root.github.io',
-  base: '/votp-website',
+  //base: '/votp-website', // Uncomment this line if deploying to GitHub Pages without a custom domain mapping to it
 });
